bcce074cd62bedf6277cd02e1a3502147865a35e
Matheus Paulino <paulino.matheus@outlook.com.br>
2026-06-03 13:09:52 -0300

    Bad Solution

--- Exercicios/Tratamento de Exceções/Exemplo1/src/application/Program.java
+++ Exercicios/Tratamento de Exceções/Exemplo1/src/application/Program.java
@@ -12,17 +12,16 @@ public class Program {
         Scanner sc = new Scanner(System.in);
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-        System.out.println("Room number: ");
+        System.out.print("Room number: ");
         int number = sc.nextInt();
         sc.nextLine();
-        System.out.println("Check-in date (dd/mm/yyyy): ");
+        System.out.print("Check-in date (dd/mm/yyyy): ");
         Date checkIn = sdf.parse(sc.next());
-        System.out.println("Check-in date (dd/mm/yyyy): ");
+        System.out.print("Check-out date (dd/mm/yyyy): ");
         Date checkOut = sdf.parse(sc.next());
 
         if (!checkOut.after(checkIn)) {
             System.out.println("Error in reservation: Check-out date must be after check-in date");
-
         }
         else {
             Reservation reservation = new Reservation(number, checkIn, checkOut);
@@ -32,18 +31,14 @@ public class Program {
             System.out.println("Enter data to update te reservation: ");
             System.out.println("Check-in date (dd/mm/yyyy): ");
             checkIn = sdf.parse(sc.next());
-            System.out.println("Check-in date (dd/mm/yyyy): ");
+            System.out.println("Check-out date (dd/mm/yyyy): ");
             checkOut = sdf.parse(sc.next());
 
-            Date now = new Date();
-            if (checkIn.before(now) || checkOut.before(now)){
-                System.out.println("Error in reservation: Reservation dates for update must be future");
-            }
-            else if (!checkOut.after(checkIn)){
-                System.out.println("Error in reservation: Check-out date must be after check-in date");
+            String error = reservation.updateDates(checkIn, checkOut);
+            if (error != null){
+                System.out.printf("Error in reservation: " + error);
             }
             else {
-                reservation.updateDates(checkIn, checkOut);
                 System.out.println("Reservation: " + reservation);
             }
 
