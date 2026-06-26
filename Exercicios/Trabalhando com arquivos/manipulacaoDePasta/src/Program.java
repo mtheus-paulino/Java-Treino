509cbb4706ab6ff64143fb981302fd126287ae0b
Matheus Paulino <paulino.matheus@outlook.com.br>
2026-06-26 13:13:48 -0300

    Alteração para demonstrar funconalidades

---
+++ Exercicios/Trabalhando com arquivos/manipulacaoDePasta/src/Program.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a file path: ");
+        String strPath = sc.nextLine();
+
+        File path = new File(strPath);
+
+        System.out.println("getName: " + path.getName()); //pega o nome do arquivo
+        System.out.println("getParent " + path.getParent()); //pega nomente o caminho
+        System.out.println("getPath " + path.getPath()); //pega o camiho inteiro
+
+        sc.close();
+    }
+}
