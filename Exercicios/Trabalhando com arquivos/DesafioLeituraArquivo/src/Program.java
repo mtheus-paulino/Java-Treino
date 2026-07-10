46fe950687b8888270f0e941343862065e636462
Matheus Paulino <paulino.matheus@outlook.com.br>
2026-07-10 13:30:12 -0300

    Atualização dos exercicios

---
+++ Exercicios/Trabalhando com arquivos/DesafioLeituraArquivo/src/Program.java
@@ -0,0 +1,61 @@
+import entities.Product;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Product> list = new ArrayList<>();
+
+        System.out.println("Enter file path: ");
+        String sourceFileStr = sc.nextLine();
+
+        File sourceFile = new File(sourceFileStr);
+        String sourcerFolderStr = sourceFile.getParent();
+
+        boolean sucess = new File(sourcerFolderStr + "\\out").mkdir();
+
+       String targetFileStr = sourcerFolderStr + "\\out\\sumary.csv";
+
+       try (BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))) {
+           String itemCsv = br.readLine();
+           while (itemCsv != null){
+
+               String[] fields = itemCsv.split(",");
+               String name = fields[0];
+               double price = Double.parseDouble(fields[1]);
+               int quantity = Integer.parseInt(fields[2]);
+
+               list.add(new Product(name, price, quantity));
+
+               itemCsv = br.readLine();
+           }
+
+           try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))) {
+
+               for (Product item: list) {
+                   bw.write(item.getName() + ", " + String.format("%.2f", item.total()));
+                   bw.newLine();
+               }
+
+               System.out.println(targetFileStr + " CREATED");
+
+           }catch (IOException e){
+               System.out.println("Error writhing file: " + e.getMessage());
+           }
+
+       }catch (IOException e){
+           System.out.println("Error writing file " + e.getMessage());
+       }
+
+
+       sc.close();
+
+    }
+}
